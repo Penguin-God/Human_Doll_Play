8f3db28657f46c535bfd0a536b4d794c41bdae6d
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-12-09 22:33:50 +0900

    chore: tmprot 폴더 정리

--- Human Doll Play/Assets/1_Scripts/Initailizer/DoTest.cs
+++ Human Doll Play/Assets/1_Scripts/Initailizer/DoTest.cs
@@ -1,3 +1,4 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,15 @@ public class DoTest : MonoBehaviour
 {
     [SerializeField] CharacterMover characterMover;
     [SerializeField] UI_Dialogue dialogue;
+
     void Start()
     {
         characterMover.DependencyInject(new GridMoveUseCase(GameSettings.TileSize), 5);
-        characterMover.Move(new Direction[] { Direction.Up, Direction.Up, Direction.Left, Direction.Left, Direction.Down, Direction.Right});
 
-        StartCoroutine( new Dialoguer(new string[] { "dd", "ss", "ff" }, dialogue, dialogue).Execute());
+        var mover = new CharacterMoveActor(characterMover, new Direction[] { Direction.Up, Direction.Up, Direction.Left, Direction.Left });
+        var dialoguer = new Dialoguer(new string[] { "dd", "ss", "ff" }, dialogue, dialogue);
+        var mover2 = new CharacterMoveActor(characterMover, new Direction[] { Direction.Down, Direction.Right, Direction.Left, Direction.Left, Direction.Left, Direction.Left });
+
+        GetComponent<SecnarioDirector>().Shooting(new IAct[] { mover, dialoguer, mover2 });
     }
 }
