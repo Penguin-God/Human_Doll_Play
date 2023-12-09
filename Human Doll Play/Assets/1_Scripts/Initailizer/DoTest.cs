fa5b0193b9170fb5964d4e3e15fb278e1215222e
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-12-09 23:27:09 +0900

    docs: 이것저것

--- Human Doll Play/Assets/1_Scripts/Initailizer/DoTest.cs
+++ Human Doll Play/Assets/1_Scripts/Initailizer/DoTest.cs
@@ -7,15 +7,32 @@ public class DoTest : MonoBehaviour
 {
     [SerializeField] CharacterMover characterMover;
     [SerializeField] UI_Dialogue dialogue;
+    [SerializeField] SecnarioDirector secnarioDirector;
 
     void Start()
     {
         characterMover.DependencyInject(new GridMoveUseCase(GameSettings.TileSize), 5);
+    }
 
-        var mover = new CharacterMoveActor(characterMover, new Direction[] { Direction.Up, Direction.Up, Direction.Left, Direction.Left });
-        var dialoguer = new Dialoguer(new string[] { "dd", "ss", "ff" }, dialogue, dialogue);
-        var mover2 = new CharacterMoveActor(characterMover, new Direction[] { Direction.Down, Direction.Right, Direction.Left, Direction.Left, Direction.Left, Direction.Left });
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            var mover = new CharacterMoveActor(characterMover, new Direction[] { Direction.Up, Direction.Up, Direction.Up, Direction.Up });
+            var dialoguer = new Dialoguer(new string[] { "¾È³ç", "µå·¡" }, dialogue, dialogue);
+            var mover2 = new CharacterMoveActor(characterMover, new Direction[] { Direction.Down, Direction.Down, Direction.Down, Direction.Down});
 
-        GetComponent<SecnarioDirector>().Shooting(new IAct[] { mover, dialoguer, mover2 });
+            secnarioDirector.Shooting(new IAct[] { mover, dialoguer, mover2 });
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            characterMover.DependencyInject(new GridMoveUseCase(GameSettings.TileSize), 5);
+
+            var mover = new CharacterMoveActor(characterMover, new Direction[] { Direction.Up, Direction.Up, Direction.Left, Direction.Left });
+            var dialoguer = new Dialoguer(new string[] { "dd", "ss", "ff" }, dialogue, dialogue);
+            var mover2 = new CharacterMoveActor(characterMover, new Direction[] { Direction.Down, Direction.Right, Direction.Left, Direction.Left, Direction.Left, Direction.Left });
+
+            secnarioDirector.Shooting(new IAct[] { mover, dialoguer, mover2 });
+        }
     }
 }
